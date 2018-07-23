0c6a805222b9647e9b9417b5a438722f0310e83b
kcbox <kcbox.io@gmail.com>
2018-07-24 00:00:19 +0800

    Initial Commit

---
+++ ObjC_Localization/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjC_Localization
+//
+//  Created by wen-chi Chang on 2018/7/24.
+//  Copyright © 2018年 wen-chi Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ObjC_Localization/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ObjC_Localization
+//
+//  Created by wen-chi Chang on 2018/7/24.
+//  Copyright © 2018年 wen-chi Chang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
